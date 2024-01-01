fc780b86d379648ed255b7cad9e714b3a934fa91
ShivnakMahor <shivank88215@gmail.com>
2024-01-01 21:04:06 +0530

    homepage skeletion v1.1.0

---
+++ frontend/src/components/homepagecard.jsx
@@ -0,0 +1,11 @@
+function HomePageCard({link}) {
+  return (
+    <>
+      <div className="w-72 rounded-md overflow-hidden ">
+        <img src={link} alt="Error" />
+      </div>
+    </>
+  )
+}
+
+export default HomePageCard;
---
+++ frontend/src/components/homepagecard2.jsx
@@ -0,0 +1,18 @@
+function HomePageCard2(){
+  return(
+    <>
+    <div>
+      <div className="flex flex-col w-4/5">
+        <span>LeetCode Contest</span>
+        Participate and win your prizes
+      </div>
+      <button>Join Contest</button>
+    </div>
+    <div className="w-20">
+      <img src="https://leetcode.com/static/images/LeetCode_Cup.png" alt="Error" />
+    </div>
+    </>
+  )
+}
+
+export default HomePageCard2;

f8bd16c851ab7f9626f8f166469ed076325e2f03
Tarun Kumar <tarun07hr@gmail.com>
2024-01-04 23:42:41 +0530

    problem page updated
    _problem page update kr dia

---
+++ frontend/src/components/problem_dropdown_button.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+function P_dropdown(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleDropdown() {
+      setIsOpen(!isOpen);}
+    return(
+        <div className="relative flex-1 inline-block text-left">
+                        <button
+                            type="button"
+                            onClick={toggleDropdown}
+                            className="inline-flex justify-center bg-gray-100 rounded items-center p-2 text-sm font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                        >
+                            Dropdown
+                            <svg
+                                className={`ml-2 h-5 w-5 transition-transform ${isOpen ? 'transform rotate-180' : ''
+                                    }`}
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor"
+                            >
+                                <path fill-rule="evenodd" d="M4.929 7.913l7.078 7.057 7.064-7.057a1 1 0 111.414 1.414l-7.77 7.764a1 1 0 01-1.415 0L3.515 9.328a1 1 0 011.414-1.414z" clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+
+                        {isOpen && (
+                            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <div className="py-1">
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    >
+                                        Item 1
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    >
+                                        Item 2
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    >
+                                        Item 3
+                                    </a>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+    )
+}
+export default P_dropdown
--- frontend/src/components/problems.jsx
+++ frontend/src/components/problems.jsx
@@ -1,25 +1,27 @@
+import P_cards from "./problem_cards1";
+import P_buttons from "./problem_button_pill";
+import P_dropdown from "./problem_dropdown_button";
 
 
 function Problems() {
+
     return (
-        <div className="grid grid-cols-4 gap-4 h-screen p-4 bg-gray-200">
+        <div className="grid grid-cols-4 gap-4 h-screen p-4 bg-gray-200 px-20">
 
             {/* First child div with 3 columns */}
             <div className="col-span-3 bg-white p-8 grid grid-cols-3 gap-4">
                 {/* Div with a column space of 3 */}
                 <div className="col-span-3 grid grid-cols-3 gap-4" id="container">
-                    <div className="col-span-1 ">
-                       <a href=""> <img className="h-full" src="https://assets.leetcode.com/users/images/7b7c1fab-b877-47fa-bc72-548fbcc9f94a_1703037199.2794356.jpg" alt="problem page image" /></a>
+                    <div className="col-span-1 rounded-md ">
+                        <a href=""> <img className=" rounded-md " src="https://assets.leetcode.com/users/images/7b7c1fab-b877-47fa-bc72-548fbcc9f94a_1703037199.2794356.jpg" alt="problem page image" /></a>
                     </div>
 
                     <div className="col-span-1 ">
-                    <a href=""> <img className="h-full" src="https://assets.leetcode.com/users/images/49479bba-73b3-45d2-9272-99e773d784b2_1687290663.3168745.jpeg" alt="problem page image" /></a>
-
+                        <a href=""> <img className=" rounded-md h-full" src="https://assets.leetcode.com/users/images/49479bba-73b3-45d2-9272-99e773d784b2_1687290663.3168745.jpeg" alt="problem page image" /></a>
                     </div>
 
                     <div className="col-span-1 ">
-                    <a href=""> <img className="h-full" src="https://assets.leetcode.com/users/images/770789b0-c96b-4663-86d1-baab25534864_1669795265.8012726.png" alt="problem page image" /></a>
-
+                        <a href=""> <img className=" rounded-md h-full" src="https://assets.leetcode.com/users/images/770789b0-c96b-4663-86d1-baab25534864_1669795265.8012726.png" alt="problem page image" /></a>
                     </div>
 
 
@@ -31,49 +33,65 @@ function Problems() {
                         <p className=" text-2xl text-neutral-600">Study plan</p>
                         <a href="">See all</a>
                     </div>
-                    <div className="col-span-1 bg-blue-200 p-4">
-                        <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
-
-                    <div className="col-span-1 bg-green-200 p-4">
-                        <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
-
-                    <div className="col-span-1 bg-yellow-200 p-4">
-                        <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
-                    <div className="col-span-1 bg-blue-200 p-4">
-                    <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
-
-                    <div className="col-span-1 bg-green-200 p-4">
-                    <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
-
-                    <div className="col-span-1 bg-yellow-200 p-4">
-                    <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
-                    </div>
+                    <P_cards></P_cards>
+                    <P_cards></P_cards>
+                    <P_cards></P_cards>
+                    <P_cards></P_cards>
+                    <P_cards></P_cards>
+                    <P_cards></P_cards>
                 </div>
                 {/* Additional divs with a column space of 3 */}
                 <div className="col-span-3 bg-blue-200 p-4">
-                <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
+                    <h2 className="text-lg font-bold mb-2"></h2>
+                    <p></p>
                 </div>
 
-                <div className="col-span-3 bg-green-200 p-4">
-                <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
+                <div className="col-span-3">
+                    <div className="flex content-center overflow-hidden overflow-x-scroll no-scrollbar gap-4 ">
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                        <P_buttons></P_buttons>
+                    </div>
+
                 </div>
 
-                <div className="col-span-3 bg-yellow-200 p-4">
-                <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
+                <div className="col-span-3">
+                    <div className="mb-3 flex flex-col">
+                        <div className="flex w-full flex-wrap gap-2">
+                            <P_dropdown />
+                            <P_dropdown />
+                            <P_dropdown />
+                            <P_dropdown />
+                            <div className="relative flex-1">
+                                <div className="flex items-center bg-gray-100 rounded p-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    width="1em"
+                                    height="1em"
+                                    fill="currentColor"
+                                    className="w-4 h-4 text-gray-500"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5.527 5.527a7.5 7.5 0 0111.268 9.852l3.581 3.583a1 1 0 01-1.414 1.415l-3.582-3.583A7.501 7.501 0 015.527 5.527zm1.414 1.414a5.5 5.5 0 107.779 7.779A5.5 5.5 0 006.94 6.94z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
+                                <input
+                                    type="text"
+                                    placeholder="Search questions"
+                                    className="outline-none ml-2 flex-grow bg-gray-100"
+                                    autoComplete="off"
+                                />
+                            </div></div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -81,10 +99,10 @@ function Problems() {
             <div className="col-span-1 bg-gray-300 p-8">
                 {/* Content for the second div */}
                 <h2 className="text-lg font-bold mb-2"></h2>
-                        <p></p>
+                <p></p>
             </div>
         </div>
     );
 }
 
-export default Problems;
+export default Problems

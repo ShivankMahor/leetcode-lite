25b18952324ee0e7463ea3c653393ef318ae557c
ShivnakMahor <shivank88215@gmail.com>
2024-01-07 14:53:46 +0530

    Router Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,50 +2,57 @@ import Discuss from "./components/discuss";
 import Homepage from "./components/homepage";
 import Explore from "./components/explore";
 import Contest from "./components/contest";
-import {createBrowserRouter,Router,RouterProvider} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Problems from "./components/problems";
 import Loginpage from './components/loginpage';
 import Registerpage from './components/registerpage';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Loginpage></Loginpage>
-  },
-  {
-    path: "/homepage",
-    element: <Homepage></Homepage>
-  },
-  {
-    path: "/discuss",
-    element: <Discuss></Discuss>
-  },
-  {
-    path: "/explore",
-    element: <Explore></Explore>
-  },
-  {
-    path: "/contest",
-    element: <Contest></Contest>
-  },
-  {
-    path: "/problem",
-    element: <Problems></Problems>
-  },
-  {
-    path: "/register",
-    element: <Registerpage></Registerpage>
-  },
-  {
-    path: "*",
-    element: <div>PageNotFound</div>
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Loginpage></Loginpage>
+//   },
+//   {
+//     path: "/homepage",
+//     element: <Homepage></Homepage>
+//   },
+//   {
+//     path: "/discuss",
+//     element: <Discuss></Discuss>
+//   },
+//   {
+//     path: "/explore",
+//     element: <Explore></Explore>
+//   },
+//   {
+//     path: "/contest",
+//     element: <Contest></Contest>
+//   },
+//   {
+//     path: "/problem",
+//     element: <Problems></Problems>
+//   },
+//   {
+//     path: "/register",
+//     element: <Registerpage></Registerpage>
+//   },
+//   {
+//     path: "*",
+//     element: <div>PageNotFound</div>
+//   }
+// ])
 
 function App() {
   return (
-    <main>
-      <RouterProvider router={router}></RouterProvider>
-    </main>
+    <Routes>
+      <Route path="/" element={<Loginpage/>}/>
+      <Route path="/explore" element={<Explore/>}/>
+      <Route path="/homepage" element={<Homepage/>}/>
+      <Route path="/discuss" element={<Discuss/>}/>
+      <Route path="/contest" element={<Contest/>}/>
+      <Route path="/problem" element={<Problems/>}/>
+      <Route path="/register" element={<Registerpage/>}/>
+      <Route path="*" element={<h1>Page Not Found</h1>}/>
+    </Routes>
   );
 }
 
